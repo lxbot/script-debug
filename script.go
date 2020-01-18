1f206b90413b401e045abd6a2403c0adfdbc25ba
もっへもへ <mohemohe@ghippos.net>
2020-01-19 02:36:13 +0900

    最終メッセージを取得するサンプル

--- script.go
+++ script.go
@@ -1,15 +1,29 @@
 package main
 
 import (
+	"fmt"
+	"plugin"
 	"strings"
 )
 
 type M = map[string]interface{}
 
+var store *plugin.Plugin
+
+func Boot(s *plugin.Plugin, c *chan M) {
+	store = s
+}
+
 func OnMessage() []func(M) M {
 	return []func(M) M{
 		func(msg M) M {
-			if strings.HasPrefix(msg["message"].(M)["text"].(string), "reply ") {
+			text := msg["message"].(M)["text"].(string)
+			last := get(msg)
+			set(msg, text)
+			if last != "" {
+				msg["message"].(M)["text"] = "last: "+last+"\n"+text
+			}
+			if strings.HasPrefix(text, "reply ") {
 				msg["mode"] = "reply"
 			} else {
 				msg["mode"] = "send"
@@ -17,4 +31,32 @@ func OnMessage() []func(M) M {
 			return msg
 		},
 	}
+}
+
+func get(m M) string {
+	fn, err := store.Lookup("Get")
+	if err != nil {
+		_ = fmt.Errorf("%v", err)
+		return ""
+	}
+	result := fn.(func(string) interface{})(key(m))
+	if result == nil {
+		return ""
+	}
+	return result.(string)
+}
+
+func set(m M, value string) {
+	fn, err := store.Lookup("Set")
+	if err != nil {
+		_ = fmt.Errorf("%v", err)
+		return
+	}
+	fn.(func(string, interface{}))(key(m), value)
+}
+
+func key(m M) string {
+	roomID := m["room"].(M)["id"].(string)
+	userID := m["user"].(M)["id"].(string)
+	return "last_msg_" + roomID + "_" + userID
 }

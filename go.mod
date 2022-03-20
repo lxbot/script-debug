c1dcf30d44d72dc435b1b90c51ce944207c03742
mohemohe <mohemohe@ghippos.net>
2022-03-21 05:52:17 +0900

    v2

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/lxbot/script-debug
 
 go 1.13
+
+require github.com/lxbot/lxlib/v2 v2.0.0-alpha.18
--- script.go
+++ script.go
@@ -1,62 +1,20 @@
 package main
 
 import (
-	"fmt"
-	"plugin"
-	"strings"
+	"github.com/lxbot/lxlib/v2"
+	"github.com/lxbot/lxlib/v2/common"
 )
 
-type M = map[string]interface{}
+func main() {
+	script, messageCh := lxlib.NewScript()
 
-var store *plugin.Plugin
-
-func Boot(s *plugin.Plugin, c *chan M) {
-	store = s
-}
-
-func OnMessage() []func(M) M {
-	return []func(M) M{
-		func(msg M) M {
-			text := msg["message"].(M)["text"].(string)
-			last := get(msg)
-			set(msg, text)
-			if last != "" {
-				msg["message"].(M)["text"] = "last: "+last+"\n"+text
-			}
-			if strings.HasPrefix(text, "reply ") {
-				msg["mode"] = "reply"
-			} else {
-				msg["mode"] = "send"
-			}
-			return msg
-		},
+	for {
+		message := <-*messageCh
+		response, err := message.Copy()
+		if err != nil {
+			common.ErrorLog("message copy error:", err)
+			continue
+		}
+		script.SendMessage(response.Reply().AddContent("script-debug"))
 	}
-}
-
-func get(m M) string {
-	fn, err := store.Lookup("Get")
-	if err != nil {
-		_ = fmt.Errorf("%v", err)
-		return ""
-	}
-	result := fn.(func(string) interface{})(key(m))
-	if result == nil {
-		return ""
-	}
-	return result.(string)
-}
-
-func set(m M, value string) {
-	fn, err := store.Lookup("Set")
-	if err != nil {
-		_ = fmt.Errorf("%v", err)
-		return
-	}
-	fn.(func(string, interface{}))(key(m), value)
-}
-
-func key(m M) string {
-	roomID := m["room"].(M)["id"].(string)
-	userID := m["user"].(M)["id"].(string)
-	return "last_msg_" + roomID + "_" + userID
 }

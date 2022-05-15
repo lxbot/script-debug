cf41ec21ddd616f31355c82b8768f6531c0d7d61
mohemohe <mohemohe@ghippos.net>
2022-05-16 05:03:27 +0900

    wip

--- script.go
+++ script.go
@@ -5,6 +5,8 @@ import (
 	"github.com/lxbot/lxlib/v2/common"
 )
 
+type M = map[string]interface{}
+
 func main() {
 	script, messageCh := lxlib.NewScript()
 
@@ -15,6 +17,15 @@ func main() {
 			common.ErrorLog("message copy error:", err)
 			continue
 		}
-		script.SendMessage(response.Reply().AddContent("script-debug"))
+
+		key := "last_msg_" + message.User.ID
+
+		lastMessage := script.GetStorage(key)
+		if lastMessage == nil {
+			lastMessage = ""
+		}
+		script.SetStorage(key, message.Contents[0].Text)
+
+		script.SendMessage(response.Reply().ResetContents().AddContent("last message: '" + lastMessage.(string) + "'"))
 	}
 }
